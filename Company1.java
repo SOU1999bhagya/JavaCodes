33cf387d252317248b553243d43e5e5038c00997
SOU1999bhagya <soubhagyalaxmivc.xworkz@gmail.com>
2021-10-08 15:48:15 +0530

    Java Files

---
+++ Company1.java
@@ -0,0 +1,34 @@
+class Company1{
+String name;
+String founder;
+int est;
+int noofemployees;
+String headquarter;
+float revenue;
+int noofBranches;
+
+void setName(String name){
+	this.name=name;
+}
+void setFounder(String founder){
+	this.founder=founder;
+}
+void setEst(int est){
+	this.est=est;
+}
+void setNoofEmployees(int noofemployees){
+	this.noofemployees=noofemployees;
+}
+void setHeadQuarter(String headquarter){
+	this.headquarter=headquarter;
+}
+void setRevenue(float revenue){
+	this.revenue=revenue;
+}
+void setNoofBranches(int noofBranches){
+	this.noofBranches=noofBranches;
+}
+
+
+
+}
---
+++ MiPhone.java
@@ -0,0 +1,25 @@
+class MiPhone{
+String modelName;
+float price;
+String colour;
+int imei;
+PhonePay phonepay;
+
+void setModelName(String modelName){
+
+    this.modelName=modelName;
+}
+
+void setPrice(float price){
+    this.price=price;
+}
+void setColour(String colour){
+    this.colour=colour;
+}
+void setImei(int imei){
+    this.imei=imei;
+}
+void setPhonepay(PhonePay phonepay){
+    this.phonepay=phonepay;
+}
+}
